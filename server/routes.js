85e4ab23008e00ed13fb9d78bf81c869e5992fa4
Mhayk Whandson <hi@mhayk.com>
2022-03-19 08:07:15 +0000

    a handler error has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- server/routes.js
+++ server/routes.js
@@ -60,7 +60,19 @@ async function routes(request, response) {
     return response.end()
 }
 
+function handleError(error, response) {
+    if (error.message.includes('ENOENT')) {
+        logger.warn(`asset not found: ${error.stack}`);
+        response.writeHead(404)
+        return response.end()
+    }
+
+    logger.error(`caught error on API ${error.stack}`);
+    response.writeHead(500)
+    return response.end()
+}
+
 export function handler(request, response) {
     return routes(request, response)
-        .catch(error => logger.error(`Deu ruimmm: ${error.stack}`));
+        .catch((error) => handleError(error, response));
 }

c8072ae64ba04d0890861115899b62dae57d8f35
Mhayk Whandson <hi@mhayk.com>
2022-03-20 14:32:04 +0000

    some new config params have been added
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- server/config.js
+++ server/config.js
@@ -5,6 +5,7 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 const root = join(currentDir, '../')
 const audioDirectory = join(root, 'audio')
 const publicDirectory = join(root, 'public')
+const songsDirectory = join(audioDirectory, 'songs')
 
 export default {
     port: process.env.PORT || 3001,
@@ -27,8 +28,13 @@ export default {
             '.html': 'text/html',
             '.css': 'text/css',
             '.js': 'application/javascript',
-        }
-    }
+        },
+        audioMediaType: 'mp3',
+        songVolume: '0.99',
+        fallbackBitRate: '128000',
+        bitRateDivisor: 8,
+        englishConversation: join(songsDirectory, "conversation.mp3"),
+    },
 }
 
 
